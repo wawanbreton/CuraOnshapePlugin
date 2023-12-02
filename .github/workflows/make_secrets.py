d5019ee08ed25bf03e1b57f49207a97a2d98d1bf
Erwan MATHIEU <Erwan>
2023-12-02 23:40:19 +0100

    Don't use fernet encryption which is not included in Cura

--- .github/workflows/make_secrets.py
+++ .github/workflows/make_secrets.py
@@ -2,16 +2,17 @@
 
 import os
 import json
-from cryptography.fernet import Fernet
 
+
+def xor_encrypt_decrypt(data, key):
+    return ''.join(chr(ord(data[i]) ^ ord(key[i % len(key)])) for i in range(len(data)))
 
 key = os.getenv('ENCRYPTION_KEY')
 client_id = os.getenv('ONSHAPE_OAUTH_CLIENT_ID')
 client_secret = os.getenv('ONSHAPE_OAUTH_SECRET')
 
-fernet = Fernet(key)
-
 json_data = json.dumps({'client_id': client_id, 'client_secret': client_secret})
 
 with open('auth.json', 'wb') as file:
-    file.write(fernet.encrypt(json_data.encode('utf-8')))
+    json_data_encrypted = xor_encrypt_decrypt(json_data, key)
+    file.write(json_data_encrypted.encode('utf-8'))
