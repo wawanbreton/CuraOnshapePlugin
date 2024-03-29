8436c864fd9ea570cc431b3075817a882257c25e
Erwan MATHIEU <wawanbreton@gmail.com>
2024-03-29 22:54:53 +0100

    Fix translations generation with empty dst folder

--- make_translations_binaries.sh
+++ make_translations_binaries.sh
@@ -3,7 +3,7 @@
 for language_dir in `ls i18n`; do
 	if [ -d "i18n/$language_dir" ]; then
 		echo "Make binary language file $language_dir"
-		language_file="i18n/$language_dir/onshape.po"
+		mkdir -p "i18n/$language_dir/LC_MESSAGES"
 		msgfmt "i18n/$language_dir/onshape.po" -o "i18n/$language_dir/LC_MESSAGES/onshape.mo" -v
 	fi
 done
